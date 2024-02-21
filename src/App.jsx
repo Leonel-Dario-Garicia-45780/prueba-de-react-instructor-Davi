9e5480579a3396da7e2d1e4a28bbcc4bcbab949b
Leonel Garcia <gun45780@gmail.com>
2024-02-21 10:08:29 -0500

    guardado 1 de tabla_ensallo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import TablaEnsayo from './componente tabla/tabla_ensayo'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (alert("")
-  )
+  return (
+        <TablaEnsayo/>
+    )
 }
 
 export default App
---
+++ src/componente tabla/tabla_ensayo.jsx
@@ -0,0 +1,27 @@
+import "./tabla_ensayo.css"
+
+const TablaEnsayo = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Nombre</th>
+                        <th>Decir </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th><td>1</td></th>
+                        <th>Juan</th>
+                        <th>Hola</th>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </div>
+    );
+}
+
+export default TablaEnsayo;
